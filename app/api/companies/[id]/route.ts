afc51018254b123ba7faadfc629fe6e17e907e90
Moses Fawole <mosesfawole2001@gmail.com>
2026-03-17 20:07:08 +0100

    another fix

--- app/api/companies/[id]/route.ts
+++ app/api/companies/[id]/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { auth } from "@/auth";
-import { supabase } from "@/lib/supabse";
+import { supabase } from "@/lib/supabase";
 
 export async function DELETE(
   req: NextRequest,
